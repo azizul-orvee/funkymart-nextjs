1b8e0a8de510066290a640fe4014bb629401155c
new name <new email>
2022-03-02 23:47:19 +0600

    header footer added

--- src/components/pages/Home/sections/Footer.js
+++ src/components/pages/Home/sections/Footer.js
@@ -1,5 +1,46 @@
-import React from "react";
+import React from 'react';
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className='flex justify-center'>
+      <div className='bg-[#ffeef2] my-20 rounded-[20px] container align-middle'>
+        <div className='flex justify-between p-24'>
+          <div className='flex gap-14'>
+            <div>funky logo</div>
+            <div className='flex flex-col'>
+              <div className='font-promomedium text-[#2f3292]'>About</div>
+              <div className='font-promomedium text-[#2f3292]'>Explore</div>
+              <div className='font-promomedium text-[#2f3292]'>My NFT's'</div>
+            </div>
+
+            <div className='flex flex-col'>
+              <div className='font-promomedium text-[#2f3292]'>Donutville</div>
+              <div className='font-promomedium text-[#2f3292]'>Funky Mart</div>
+              <div className='font-promomedium text-[#2f3292]'>The Bakery</div>
+              <div className='font-promomedium text-[#2f3292]'>Town Hall</div>
+              <div className='font-promomedium text-[#2f3292]'>
+                Funky Tavern
+              </div>
+            </div>
+            <div className='flex flex-col'>
+              <div className='font-promomedium text-[#2f3292]'>FAQ</div>
+              <div className='font-promomedium text-[#2f3292]'>Stacks</div>
+              <div className='font-promomedium text-[#2f3292]'>Hiro Wallet</div>
+            </div>
+          </div>
+
+          <div className='flex flex-col gap-12'>
+            <div className='flex gap-5'>
+              <div>twitter</div>
+              <div>discord</div>
+            </div>
+
+            <button className='connect-wallet-btn font-PoppinsRegular'>
+              Connect Wallet
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/components/pages/Home/sections/Navbar.js
+++ src/components/pages/Home/sections/Navbar.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React from 'react';
 
 export const Navbar = () => {
   return (
     <div>
-      <div className="flex">
-        <div>Funky Logo</div>
-        <div>About</div>
-        <div>Explore</div>
-        <div>Donutvile</div>
-        <div>My NFT's</div>
+      <div className='flex justify-between items-center h-[88px] bg-white'>
+        <div className='flex gap-10 p-28 items-center'>
+          <div>Funky Logo</div>
+          <div className='font-poppinsregular text-[#2f3292]'>About</div>
+          <div className='font-poppinsregular text-[#2f3292]'>Explore</div>
+          <div className='font-poppinsregular text-[#2f3292]'>Donutvile</div>
+          <div className='font-poppinsregular text-gray-500'>My NFT's</div>
+        </div>
+        <div className='flex gap-10 p-28 items-center'>
+          <div>twitter</div>
+          <div>discord</div>
+          <div>
+            <button className='connect-wallet-btn font-PoppinsRegular'>Connect Wallet</button>
+          </div>
+        </div>
       </div>
     </div>
   );
